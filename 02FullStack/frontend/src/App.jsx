1c3973f26087ab781593bf528aae445919158b84
VINODvoid <iamlearner.v@gmail.com>
2024-02-14 07:13:27 +0530

    update :  proxy url

--- 02FullStack/frontend/src/App.jsx
+++ 02FullStack/frontend/src/App.jsx
@@ -1,39 +1,31 @@
-/* eslint-disable no-unused-vars */
-import axios from 'axios'
-import { useEffect, useState } from 'react'
-import './App.css'
+import { useEffect,useState } from "react";
+import "./App.css";
+import axios from "axios";
 function App() {
-
-
-  const [jokes, setJokes] = useState([])
-
-  useEffect(()=>{
-    axios.get('/api/jokes')
-    .then((res)=>{
-      setJokes(res.data)
-    })
-    
-    .catch((error)=>{
-      console.error(error);
-    })
-
-  })
-
-
+  const [jokes, setJokes] = useState([]);
+  useEffect(() => {
+    axios
+      .get("/api/jokes")
+      .then((response) => {
+        setJokes(response.data);
+      })
+      .catch((error) => {
+        console.log(error);
+      });
+  }, []);
   return (
     <>
-     <h1>FrontEnd And BackEnd</h1>
-     <p>Jokes : {jokes.length}</p>
-     {
-      jokes.map((joke, index) => (
+      <h1>Learn : TRY and FAIL </h1>
+      <p>JOKES : {jokes.length}</p>
+      {jokes.map((joke) => (
         <div key={joke.id}>
+          <h2>{joke.id}</h2>
           <h3>{joke.title}</h3>
           <p>{joke.content}</p>
         </div>
-      ))
-     }
+      ))}
     </>
-  )
+  );
 }
 
-export default App
+export default App;
